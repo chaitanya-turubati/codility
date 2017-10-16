22acd7aece265a1bf76732d96231b7a970203915
chaitanya-turubati <chaitanya.turubati@gmail.com>
2017-10-16 17:59:18 +0530

    Add as a maven project

---
+++ src/main/java/io/chatty/boron2013/Solution.java
@@ -0,0 +1,13 @@
+package io.chatty.boron2013;
+
+public class Solution {
+
+  public int solution(int[] A) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    int[] A = {1, 5, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2};
+    System.out.println((new Solution()).solution(A));
+  }
+}
